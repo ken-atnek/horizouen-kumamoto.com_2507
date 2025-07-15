1e728918a826b912ce156f498dad979018152be9
Kenta Urakami <ken.atnek@gmail.com>
2025-07-15 15:34:07 +0900

    faviconパス調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,9 @@
 
 import type { Metadata } from 'next';
 import '@/styles/globals.scss';
-import { Noto_Sans_JP } from 'next/font/google';
 import Header from '@/components/common/Header';
 import Footer from '@/components/common/Footer';
+import { Noto_Sans_JP } from 'next/font/google';
 const notoSans = Noto_Sans_JP({
   subsets: ['latin'],
   weight: ['100', '300', '400', '500', '700', '900'],
@@ -46,15 +46,15 @@ export const metadata: Metadata = {
   robots: isRealProduction ? 'index, follow' : 'noindex, nofollow',
   icons: {
     icon: [
-      // { url: 'favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      // { url: 'favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: 'favicon/favicon.ico', type: 'image/x-icon' },
+      // { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      // { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon/favicon.ico', type: 'image/x-icon' },
     ],
-    // apple: [{ url: 'favicon/apple-touch-icon.png', sizes: '180x180' }],
+    // apple: [{ url: '/favicon/apple-touch-icon.png', sizes: '180x180' }],
     // other: [
     //   {
     //     rel: 'icon',
-    //     url: 'favicon/android-chrome-192x192.png',
+    //     url: '/favicon/android-chrome-192x192.png',
     //     sizes: '192x192',
     //     type: 'image/png',
     //   },
